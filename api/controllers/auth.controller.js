7d71057902af9aee8bb5e968d3b80ead6ebb47a2
Jawid Fakhari <jawid_fakhari@live.it>
2024-05-12 22:59:38 +0200

    create signup route + hashed password

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,32 @@
+import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs'
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (
+    !"username" ||
+    !"email" ||
+    !"password" ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  //usiamo npm i bcryptjs package per creare hashed password, cosi non nessuno può avere a disposizione il password ne anche admin.
+  const hashedPassword = bcryptjs.hashSync(password, 10); // metodo per creare hashed password
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword // ora il password è un hashed password
+  });
+  try {
+    await newUser.save();
+    res.json("Signup successful");
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
