359053ab41ff3e5457d8e34080467ed748a03e1a
Jawid Fakhari <jawid_fakhari@live.it>
2024-05-27 23:29:30 +0200

    bug searching!!!!

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,22 +1,29 @@
 import { Timestamp } from "bson";
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default:
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&psig=AOvVaw2C9udd_fKAdFGLHlv4v4OJ&ust=1716586698763000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNDckPXdpIYDFQAAAAAdAAAAABAE",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-}, {timestamps: true}
+  { timestamps: true }
 );
 
 const User = mongoose.model('User', userSchema);
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -8,4 +8,6 @@ router.post("/signup", signup);
 
 router.post("/signin", signin);
 
+router.post('/google', google);
+
 export default router;
